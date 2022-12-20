e124bdffb64255a13b526e96ebfefcec08cb4f7f
cyanfhine <yifan03.chen@tum.de>
2022-12-20 22:33:33 +0100

    pathlength implementiert

--- src/pgdp/pingutrip/PinguTrip.java
+++ src/pgdp/pingutrip/PinguTrip.java
@@ -51,7 +51,9 @@ final public class PinguTrip {
 
     public static double pathLength(Stream<OneWay> oneWays) {
         // TODO: Task 3
-        return 0.0;
+        return oneWays
+                .map(way -> way.getLength())
+                .reduce(0.0, (acc, wl) -> acc + wl);
     }
 
     public static List<OneWay> kidFriendlyTrip(List<OneWay> oneWays) {
@@ -76,29 +78,29 @@ final public class PinguTrip {
 
     public static void main(String[] args) {
         List<WayPoint> wayPoints = readWayPoints("test_paths/path.txt").toList();
-        System.out.println(wayPoints);
-         //List.of(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2));
+        //List.of(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2));
 
-        /*List<OneWay> oneWays = transformToWays(wayPoints).toList();
-        // List.of(new OneWay(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2)));
+        //List<OneWay> oneWays = transformToWays(wayPoints).toList();
+        List<OneWay> oneWays = List.of(new OneWay(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2)));
 
         double length = pathLength(oneWays.stream());
+        System.out.println(length);
         // 17.230 ...
 
-        List<OneWay> kidFriendly = kidFriendlyTrip(oneWays);
+        //List<OneWay> kidFriendly = kidFriendlyTrip(oneWays);
         // List.of(new OneWay(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2)));
 
-        WayPoint furthest = furthestAwayFromHome(wayPoints.stream(), wayPoints.get(0));
+        //WayPoint furthest = furthestAwayFromHome(wayPoints.stream(), wayPoints.get(0));
         // new WayPoint(19.1, 3.2);
 
-        boolean onTheWay = onTheWay(oneWays.stream(), new WayPoint(0.0, 0.0));
+        //boolean onTheWay = onTheWay(oneWays.stream(), new WayPoint(0.0, 0.0));
         // false
 
-        onTheWay = onTheWay(oneWays.stream(), new WayPoint(19.1, 3.2));
+        //onTheWay = onTheWay(oneWays.stream(), new WayPoint(19.1, 3.2));
         // true
 
-        String directions = prettyDirections(oneWays.stream());
-        // "25 Schritte Richtung 331 Grad."*/
+        //String directions = prettyDirections(oneWays.stream());
+        // "25 Schritte Richtung 331 Grad."
     }
 
 }
